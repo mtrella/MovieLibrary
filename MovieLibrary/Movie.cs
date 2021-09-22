94e62bfefa45a02172eb4ad4577268746d371fe2
mtrella <89224976+mtrella@users.noreply.github.com>
2021-09-22 15:15:51 -0500

    Add files via upload

---
+++ MovieLibrary/Movie.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace MovieLibrary
+{
+    public class Movie
+    {
+        // public properties
+        public UInt64 movieId { get; set; }
+        public string title { get; set; }
+        public List<string> genres { get; set; }
+
+        // constructor
+        public Movie()
+        {
+            genres = new List<string>();
+        }
+
+        // public method
+        public string Display()
+        {
+            return $"ID: {movieId}\nTitle: {title}\nGenres: {string.Join(", ", genres)}\n";
+        }
+    }
+}
